ad0a234bdd477e63b33b24831b4dd033667c591d
Aayush kumar <aayush.21jdai066@jietjodhpur.ac.in>
2025-12-14 01:00:24 +0530

    chore(kafka): enhance Kafka client configuration and logging

--- app/kafka/KafkaClient.js
+++ app/kafka/KafkaClient.js
@@ -1,8 +1,34 @@
-require('dotenv').config();
+const { Kafka } = require('kafkajs');
+const config = require('../config/config');
+const logger = require('../utils/logger');
 
-const {Kafka} = require('kafkajs');
-
-exports.kafka = new Kafka({
-    clientId: process.env.KAFKA_GROUP_ID,
-    brokers:[process.env.KAFKA_BROKERS],
+// Create Kafka instance
+const kafka = new Kafka({
+  clientId: config.kafka.clientId,
+  brokers: config.kafka.brokers,
+  retry: {
+    initialRetryTime: 100,
+    retries: 8
+  },
+  logCreator: () => {
+    return ({ level, log }) => {
+      const { message, ...extra } = log;
+      
+      switch (level) {
+        case 1: // ERROR
+          logger.error(`[Kafka] ${message}`, extra);
+          break;
+        case 2: // WARN
+          logger.warn(`[Kafka] ${message}`, extra);
+          break;
+        case 4: // INFO
+          logger.info(`[Kafka] ${message}`, extra);
+          break;
+        default:
+          logger.debug(`[Kafka] ${message}`, extra);
+      }
+    };
+  }
 });
+
+module.exports = { kafka };
